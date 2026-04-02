cf557f06c3ea21dfa6c4ddacada8c33e69ac13a3
Thiruvirkolam <ashwinthiru20@gmail.com>
2026-04-02 09:16:05 +0530

    Delete Entity directory

--- Entity/Assignments/EntityAssignment/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace EntityAssignment.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
