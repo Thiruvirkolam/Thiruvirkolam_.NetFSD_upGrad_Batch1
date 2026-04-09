52c76dacea62bf60cba69e481a438cd7d36c181b
Thiruvirkolam <ashwinthiru20@gmail.com>
2026-04-09 20:46:12 +0530

    Delete ASP.NET CORE/Assignment directory

--- ASP.NET CORE/Assignment/Assignment1(7-14)/Assignment(7-14)/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace Assignment_7_14_.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- ASP.NET CORE/Assignment/Assignment1(7-14)/Assignment(7-14)/Models/Students.cs
+++
@@ -1,8 +0,0 @@
-namespace Assignment_7_14_.Models
-{
-    public class Student
-    {
-        public string? Name { get; set; }
-        public int Age { get; set; }
-    }
-}
